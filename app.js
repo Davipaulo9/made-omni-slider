cecf1c1202a351e8b36b314cc7d30290bbc29d17
Davi de Paulo <30410050+Davipaulo9@users.noreply.github.com>
2023-10-12 15:18:18 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from the "public" directory (where your HTML file is located).
+app.use(express.static('public'));
+
+// Start the server on port 3000.
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
